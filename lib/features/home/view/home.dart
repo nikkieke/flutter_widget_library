9906c76cd687e092d020202c3a948adf90c2f069
nikkieke <nikkieke001@gmail.com>
2024-03-11 01:18:52 +0100

    basic project setup

--- lib/features/home/view/home.dart
+++ lib/features/home/view/home.dart
@@ -4,6 +4,25 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  Scaffold(
+      drawer: Drawer(
+          width: 200,
+          backgroundColor: Colors.black,
+          child: Container(
+              padding: const EdgeInsets.only(top: 50),
+              child: Column(
+                  children: [
+                    ListTile(
+                        leading: const Icon(Icons.arrow_drop_down_circle_outlined),
+                        title: const Text("Buttons",style: TextStyle(
+                            color: Colors.white,  fontSize: 15)),
+                        onTap: (){}
+                    ),
+
+                  ]
+              )
+          )
+      ),
+    );
   }
 }
